dedbf4d57e1353a259f1dbd93ab4b6309db58804
iareec <14r33c@gmail.com>
2020-10-17 17:17:56 +0100

    Correção em resize.js

--- js/resize.js
+++ js/resize.js
@@ -1,11 +1,14 @@
 // Função para alterar a posição da imagem em relação a área visível
 function resizeWindow() {
     // A posição da imagem é alterada para "center -[((altura da imagem background-1) - área visível) a dividir por 2]px"
-    var value = "center " + "-" + (1079 - window.innerHeight) / 2 + "px";
+    if (window.innerHeight <= 1080) {
+        var value = "center " + "-" + (1079 - window.innerHeight) / 2 + "px";
+
+    } else {
+        var value = "center " + (window.innerHeight - 1079) / 2 + "px";
+    }
     document.body.style.backgroundPosition = value;
 }
 // A medida que se altera o tamanho da área visível, a função é executada
 window.onresize = resizeWindow;
 resizeWindow();
-
-// Nota: a proprieda é inútil se o nível de zoom for baixo (quando a área do background-image é inferior a área visível)
