2f69935be0a8ed57e3032af4e58b8f32dd4f323c
Cheap-Ship <pmirmida@gmail.com>
2020-10-25 00:38:00 +0100

    Players own files, optimization

---
+++ js/Players/player2.js
@@ -0,0 +1,121 @@
+export default class Player2 {
+    constructor(ctx, W, H, HH, HW) {
+        this.ctx = ctx;
+        this.W = W;
+        this.H = H;
+        this.HH = HH;
+        this.HW = HW;
+
+        // Imagens
+        
+        // Fonte das imagens das personagens (algumas foram alteradas, neste caso para o Player 2):
+        // https://www.spriters-resource.com/pc_computer/cardsagaswars/sheet/106811/
+        // Estes sprites não são para uso comercial.
+
+        this.Right = new Image();
+        this.Right.src = '../../imgs/p2right.png'; // Sprite para o Player 1 quando se move para a direita
+        this.Left = new Image();
+        this.Left.src = '../../imgs/p2left.png'; // Sprite para o Player 1 quando se move para a esquerda
+
+        this.rightKey = false; this.leftKey = false;
+        this.X = this.W / 2 + this.W / 10; // Posição do Player 1 relativa a largura do ambiente
+
+        let pRMSB = 0; // Player Right Movement Sprite and Breath
+        let pLMSB = 17; // Player Left Movement Sprite and Breath
+        let pHWRS = 2; // Player Harpoon While Right Sprite
+        let pHWLS = 16; // Player Harpoon While Left Sprite
+
+        this.RightMove = true; // Por default, os players estão virados para o lado direito,
+        this.LeftMove = false; this.Harpoon = false; // mas estas variáveis servem para detetar e efetuar determinado drawImage.
+
+        this.PRMSB = function() { // No ficheiro de sprites de movimento e respiração (para a direita)
+            pRMSB++; // são usados apenas o primeiro e o segundo.
+            if (pRMSB == 2) pRMSB = 0;
+        }
+        this.PLMSB = function() { // No ficheiro de sprites de movimento e respiração (para a esquerda)
+            pLMSB--; // são usados apenas o último e o penúltimo.
+            if (pLMSB == 15) pLMSB = 17;
+        }    
+        this.PHWRS = function() { // No ficheiro de sprites de movimento e respiração (para a direita)
+            pHWRS++; // são usados apenas o primeiro e o segundo.
+            if (pHWRS == 14) pHWRS = 2;
+        }    
+        this.PHWLS = function() { // No ficheiro de sprites de movimento e respiração (para a direita)
+            pHWLS--; // são usados apenas o primeiro e o segundo.
+            if (pHWLS == 4) pHWLS = 16;
+        }
+
+        window.addEventListener("load", this.myInit(), true); 
+        // Quando o ecrã é carregado, são acionadas as funções para o movimento e respiração das personagens.
+        //Tanto para o lado direito e esquerdo, são usados dois frames do sprite a cada segundo (Movimento e respiração)
+
+        this.sIPHWRS();
+        this.sIPHLRS();
+
+        this.GetSprite = function(sprite) {
+            switch (sprite) {
+                case "pRMSB": return pRMSB;
+                case "pLMSB": return pLMSB;
+                case "pHWRS": return pHWRS;
+                case "pHWLS": return pHWLS;
+            }
+        }
+    }
+
+    myInit() { this.sIPRMSB(), this.sIPLMSB()}
+    sIPRMSB() { setInterval(this.PRMSB, 1000 / 2)}
+    sIPLMSB() { setInterval(this.PLMSB, 1000 / 2) }
+    // 13 frames (2 to 14)
+    sIPHWRS() { // - CHAMADO APENAS UMA VEZ
+        setInterval(this.PHWRS(), 1000/26);
+    } // 13 frames (16 to 4) 
+    sIPHLRS() { // - CHAMADO APENAS UMA VEZ
+        setInterval(this.PHWLS(), 1000/26);
+    }
+
+    Desenho() {
+        this.ctx.beginPath();
+        let Dir = this.RightMove ? 
+        this.Harpoon ? this.GetSprite("pHWRS") : this.GetSprite("pRMSB")
+        : this.Harpoon ? this.GetSprite("pHWLS") : this.GetSprite("pLMSB")
+        let N = this.RightMove ? 23 : 64;
+        let Sprite = this.RightMove ? this.Right : this.Left;
+        this.ctx.drawImage(Sprite, N + (Dir + 1) + Dir * 124, 32, 37, 47, this.X - this.HW / 2, this.H - this.HH - 60, 37, 47);
+        
+        this.ctx.fillStyle = "red";
+        this.ctx.textAlign = 'center';
+        this.ctx.fillText("P2", this.X, this.H - 110 - this.GetSprite("pRMSB"));
+        this.ctx.fill();
+        this.ctx.closePath();
+        if (this.rightKey && this.X < this.W - this.HW + this.HW / 2)
+            this.X++;
+        if (this.leftKey && this.X > 0 + this.HW / 2)
+            this.X--;
+    }
+
+    ArrowPressed(e) {
+        if (e.key == 'd') {
+            this.rightKey = true;
+            this.leftKey = false;
+            this.RightMove = true;
+            this.LeftMove = false;
+            this.Harpoon = false;
+        } else if (e.key == 'a') {
+            this.leftKey = true;
+            this.rightKey = false;
+            this.LeftMove = true;
+            this.RightMove = false;
+            this.Harpoon = false;
+        } else if (e.key == 'w') {
+            this.Harpoon = true;
+        }
+    }
+    
+    ArrowReleased(e) {
+        if (e.key == 'd') {
+            this.rightKey = false;
+        } else if (e.key == 'a') {
+            this.leftKey = false;
+        }
+    }
+}
--- js/canvas.js
+++ js/canvas.js
@@ -1,22 +1,15 @@
-export default class GameCanvas{
+import Player1 from './Players/player1.js';
+import Player2 from './Players/player2.js';
+
+export default class GameCanvas {
     constructor() {
         const canvas = document.querySelector('#game');
         const ctx = canvas.getContext("2d");
-        
-        // Imagens
-        
-        // Fonte das imagens das personagens (algumas foram alteradas, neste caso para o Player 2):
-        // https://www.spriters-resource.com/pc_computer/cardsagaswars/sheet/106811/
-        // Estes sprites não são para uso comercial.
-        
-        let P1Right = new Image();
-        P1Right.src = '../imgs/p1right.png'; // Sprite para o Player 1 quando se move para a direita
-        let P1Left = new Image();
-        P1Left.src = '../imgs/p1left.png'; // Sprite para o Player 1 quando se move para a esquerda
-        let P2Right = new Image();
-        P2Right.src = '../imgs/p2right.png'; // Sprite para o Player 2 quando se move para a direita
-        let P2Left = new Image();
-        P2Left.src = '../imgs/p2left.png'; // Sprite para o Player 2 quando se move para a esquerda
+
+        const W = canvas.width;
+        const H = canvas.height;
+        const HH = 47; // Altura da Hitbox e das personagens
+        const HW = 37; // Largura da hitbox e das personages
         
         // Fonte do fundo do níveis:
         // https://imgur.com/gallery/0AnOW (contém 7 gifs)
@@ -31,79 +24,18 @@ export default class GameCanvas{
         let B3 = new Image(); // GIF 6/7 com 11 frames
         B3.src = '../imgs/background3.png'; // Valores: bIMGS 0 to 11 ; setInterval 1000 / 13
         
-        //
-        const W = canvas.width;
-        const H = canvas.height;
-        const HH = 47; // Altura da Hitbox e das personagens
-        const HW = 37; // Largura da hitbox e das personages
-        
-        let rightKey = false; let leftKey = false; let letterDKey = false; let letterAKey = false;
-        let x1 = W / 2; // Posição do Player 1 relativa a largura do ambiente (inicialmente no centro por default)
-        let x2 = W / 2; // Posição do Player 2 relativa a largura do ambiente (inicialmente no centro por default)
-        
-        window.addEventListener("load", myInit, true); // Quando o ecrã é carregado, 
-        function myInit() { sIPRMSB(), sIPLMSB(), sIBIMGS()}; // são acionadas as funções para o movimento e respiração das personagens.
-        
-        // Tanto para o lado direito e esquerdo, são usados dois frames do sprite a cada segundo (Movimento e respiração)
-        function sIPRMSB() {
-            setInterval(PRMSB, 1000 / 2);
-        };
-        
-        function sIPLMSB() {
-            setInterval(PLMSB, 1000 / 2);
-        };
-        
-        // 13 frames (2 to 14)
-        function sIPHWRS() { // - CHAMADO APENAS UMA VEZ
-            setInterval(PHWRS, 1000/26);
-        } // 13 frames (16 to 4) 
-        function sIPHLRS() { // - CHAMADO APENAS UMA VEZ
-            setInterval(PHWLS, 1000/26);
-        }
-        
-        // Timer para o background do nível
-        function sIBIMGS() {
-            setInterval(BIMGS, 1000 / 13);
-        };
-        
-        let pRMSB = 0; // Player Right Movement Sprite and Breath
-        let pLMSB = 17; // Player Left Movement Sprite and Breath
-        let pHWRS = 2; // Player Harpoon While Right Sprite
-        let pHWLS = 16; // Player Harpoon While Left Sprite
         let bIMGS = 0; // Background Image Sprite
-        
+
+        // Timer para o background do nível
+        setInterval(BIMGS, 1000 / 13)
         function BIMGS() {
             bIMGS++;
             if (bIMGS == 11)
                 bIMGS = 0;
         }
-        
-        function PRMSB() { // No ficheiro de sprites de movimento e respiração (para a direita)
-            pRMSB++; // são usados apenas o primeiro e o segundo.
-            if (pRMSB == 2)
-                pRMSB = 0;
-        }
-        
-        function PLMSB() { // No ficheiro de sprites de movimento e respiração (para a esquerda)
-            pLMSB--; // são usados apenas o último e o penúltimo.
-            if (pLMSB == 15)
-                pLMSB = 17;
-        }
-        
-        function PHWRS() { // No ficheiro de sprites de movimento e respiração (para a direita)
-            pHWRS++; // são usados apenas o primeiro e o segundo.
-            if (pHWRS == 14)
-                pHWRS = 2;
-        }
-        
-        function PHWLS() { // No ficheiro de sprites de movimento e respiração (para a direita)
-            pHWLS--; // são usados apenas o primeiro e o segundo.
-            if (pHWLS == 4)
-                pHWLS = 16;
-        }
-        
-        let P1RightMove = true; let P2RightMove = true; // Por default, os players estão virados para o lado direito,
-        let P1LeftMove = false; let P2LeftMove = false; let P1Harpoon = false; let P2Harpoon = false; // mas estas variáveis servem para detetar e efetuar determinado drawImage.
+
+        let player1 = new Player1(ctx, W, H, HH, HW);
+        let player2 = new Player2(ctx, W, H, HH, HW);
         
         // Funções
         function render() {
@@ -119,93 +51,23 @@ export default class GameCanvas{
             ctx.fillRect(0, 300, 480, 60);
             ctx.fill();
             ctx.closePath();
-            // Desenho
-            // Player 2 (primeiro se desenha esta personagem, para que o Player 1 esteja numa camada a frente)
-            ctx.beginPath();
-            if (P2RightMove && !P2Harpoon) { // Se o movimento for para a direita
-                ctx.drawImage(P2Right, 23 + (pRMSB + 1) + pRMSB * 124, 32, 37, 47, x2 - HW / 2, H - HH - 60, 37, 47);
-            } else if (P2LeftMove && !P2Harpoon) { // Se o movimento for para a esquerda
-                ctx.drawImage(P2Left, 64 + (pLMSB + 1) + pLMSB * 124, 32, 37, 47, x2 - HW / 2, H - HH - 60, 37, 47);
-            } else if (P2RightMove && P2Harpoon){
-                ctx.drawImage(P2Right, 23 + (pHWRS + 1) + pHWRS * 124, 32, 37, 47, x2 - HW / 2, H - HH - 60, 37, 47);
-            } else if (P2LeftMove && P2Harpoon){
-                ctx.drawImage(P2Left, 64 + (pHWLS + 1) + pHWLS * 124, 32, 37, 47, x2 - HW / 2, H - HH - 60, 37, 47);
-            }
-            ctx.fillStyle = "red";
-            ctx.textAlign = 'center';
-            ctx.fillText("P2", x2, H - 110 - pRMSB);
-            ctx.fill();
-            ctx.closePath();
-            if (letterDKey && x2 < W - HW + HW / 2)
-                x2++;
-            if (letterAKey && x2 > 0 + HW / 2)
-                x2--;
-            //Player 1
-            ctx.beginPath();
-            if (P1RightMove && !P1Harpoon) {
-                ctx.drawImage(P1Right, 23 + (pRMSB + 1) + pRMSB * 124, 32, 37, 47, x1 - HW / 2, H - HH - 60, 37, 47);
-            } else if (P1LeftMove && !P1Harpoon) {
-                ctx.drawImage(P1Left, 64 + (pLMSB + 1) + pLMSB * 124, 32, 37, 47, x1 - HW / 2, H - HH - 60, 37, 47);
-            } else if (P1RightMove && P1Harpoon){
-                ctx.drawImage(P1Right, 23 + (pHWRS + 1) + pHWRS * 124, 32, 37, 47, x1 - HW / 2, H - HH - 60, 37, 47);
-            } else if (P1LeftMove && P1Harpoon){
-                ctx.drawImage(P1Left, 64 + (pHWLS + 1) + pHWLS * 124, 32, 37, 47, x1 - HW / 2, H - HH - 60, 37, 47);
-            }
-            ctx.fillStyle = "blue";
-            ctx.textAlign = 'center';
-            ctx.fillText("P1", x1, H - 110 - pRMSB);
-            ctx.fill();
-            ctx.closePath();
-            if (rightKey && x1 < W - HW + HW / 2)
-                x1++;
-            if (leftKey && x1 > 0 + HW / 2)
-                x1--;
-            window.requestAnimationFrame(render);
+            player2.Desenho();
+            player1.Desenho();
+            window.requestAnimationFrame(render);            
         }
         render();
         
         function ArrowPressed(e) {
-            if (e.key == 'ArrowRight') {
-                rightKey = true;
-                leftKey = false;
-                P1RightMove = true;
-                P1LeftMove = false;
-                P1Harpoon = false;
-            } else if (e.key == 'ArrowLeft') {
-                leftKey = true;
-                rightKey = false;
-                P1LeftMove = true;
-                P1RightMove = false;
-                P1Harpoon = false;
-            } else if (e.key == 'ArrowUp'){
-                P1Harpoon = true;
-            } else if (e.key == 'd') {
-                letterDKey = true;
-                letterAKey = false;
-                P2RightMove = true;
-                P2LeftMove = false;
-            } else if (e.key == 'a') {
-                letterAKey = true;
-                letterDKey = false;
-                P2LeftMove = true;
-                P2RightMove = false;
-            } else if (e.key == 'w') {
-                P2Harpoon = true;
-            }
+            player1.ArrowPressed(e);
+            player2.ArrowPressed(e);
         }
         
         function ArrowReleased(e) {
-            if (e.key == 'ArrowRight') {
-                rightKey = false;
-            } else if (e.key == 'ArrowLeft') {
-                leftKey = false;
-            } else if (e.key == 'd') {
-                letterDKey = false;
-            } else if (e.key == 'a') {
-                letterAKey = false;
-            }
+            player1.ArrowReleased(e);
+            player2.ArrowReleased(e);
         }
         
         window.addEventListener('keydown', ArrowPressed);
-        window.addEventListener('keyup', ArrowReleased);    }
+        window.addEventListener('keyup', ArrowReleased);
+    }
 }
